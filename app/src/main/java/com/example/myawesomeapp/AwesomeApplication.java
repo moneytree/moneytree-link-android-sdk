11318ea96354051aae038ad89cb30d64eec71511
So Nakamura <snakamura@moneytree.jp>
2017-09-11 15:56:04 +0900

    Updated sample code to use new APIs

--- app/src/main/java/com/example/myawesomeapp/AwesomeApplication.java
+++ app/src/main/java/com/example/myawesomeapp/AwesomeApplication.java
@@ -2,10 +2,6 @@ package com.example.myawesomeapp;
 
 import android.app.Application;
 
-import com.getmoneytree.MoneytreeLink;
-import com.getmoneytree.MoneytreeLinkConfiguration;
-import com.getmoneytree.MoneytreeLinkScope;
-
 /**
  * @author Moneytree KK
  */
@@ -15,14 +11,6 @@ public class AwesomeApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // Initialize MTLinkClient
-        final MoneytreeLinkConfiguration configuration = new MoneytreeLinkConfiguration.Builder()
-                .isProduction(false) // true: production, false: staging
-                .clientId(getString(R.string.link_client_id))  // set your ClientId
-                .scopes(MoneytreeLinkScope.GuestRead) // set scopes
-                //.scopes("customized_scope", "new_scope") // You can add scopes using String as well.
-                .build();
-        // Set application instance and configuration that created above.
-        MoneytreeLink.init(this, configuration);
+        // Strongly recommend to initialize MoneytreeLink client here if you don't want to use both 'Implicit' and 'Code' at the same time or you don't want to set different scopes dynamically. This AwesomeApp is a show case app to demonstrate what the SDK provides, so it gives capability to change configuration after the app initializes once. That's why there are no initialization phase here.
     }
 }
