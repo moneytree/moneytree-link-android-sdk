03fe2f4761abeedbeff24be32136c5e1a072d3b5
mt-mobile <support@moneytree.jp>
2020-08-28 00:10:12 +0000

    [CI] Updated SDK to v6.0.0-alpha02

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+  apply(from = "mtlink_version.gradle.kts")
+
+  repositories {
+    google()
+    jcenter()
+  }
+
+  dependencies {
+    classpath("com.android.tools.build:gradle:4.0.1")
+    classpath("com.google.gms:google-services:4.3.3")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+  }
+}
+
+allprojects {
+  repositories {
+    google()
+    jcenter()
+  }
+}
+
+tasks.register("clean", Delete::class) {
+  delete(rootProject.buildDir)
+}
